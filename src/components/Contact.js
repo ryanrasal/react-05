c741fd41233b7c8297d27832e29b5199e18f7f8e
ryanrasal <112951711+ryanrasal@users.noreply.github.com>
2022-10-08 18:24:21 +0200

    1 file modified

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ const Contact = (props) => {
   return (
     <div className="friend-item">
       <h3>{props.name}</h3>
-      <h4>{props.email}</h4>
-      <h4>{props.phone}</h4>
+      <h4> 📧 {props.email}</h4>
+      <h4> 📞 {props.phone}</h4>
       <button>Delete</button>
     </div>
   );
